68729d6fe55d062009cef6eba8ac80a6baf1e620
Chris John <chris.john118@gmail.com>
2021-08-28 08:30:42 -0400

    Misc. bug fixes

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,15 +1,15 @@
 class ExchangeRatesController < ApplicationController
   def index
-    @exchange_rate = ExchangeRate.find_by_code(params[:base_code])
+    @exchange_rate = ExchangeRate.find_by_code(params[:baseCode])
     if @exchange_rate.nil?
-      new_rates = ExchangeRateApi.call(params[:base_code])
-      if new_rates.rates.present?
+      new_rates = ExchangeRateApi.call(params[:baseCode])
+      if new_rates&.rates.present?
         @exchange_rate = ExchangeRate.create(base_code: new_rates.base_code, rates: new_rates.rates)
       end
     end
 
-    exchange_amount = (params[:amount].to_d * @exchange_rate.rates.send(params[:exchange_code].to_sym).to_d).round(2)
+    exchange_amount = (params[:baseAmount].to_d * @exchange_rate&.rates&.send(params[:exchangeCode].to_sym).to_d).round(2)
 
-    render json: { exchange_amount: exchange_amount }
+    render json: { exchangAmount: exchange_amount }
   end
 end
